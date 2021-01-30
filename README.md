0c6ecac1bb0078be01f273b54292783eafea4f08
Miguel Ferreira <mferreira@keep.pt>
2021-01-30 20:14:13 +0000

    closes #2, #4

--- README.md
+++ README.md
@@ -18,7 +18,6 @@ This [homebridge](https://github.com/nfarina/homebridge) plugin exposes a web-ba
 2. Install the plugin by running `npm install -g homebridge-garage-door-shelly1` or by searching for `homebridge-garage-door-shelly1` on the [plugins tab](https://github.com/homebridge/homebridge#installing-plugins) if you are using [Homebridge UI](https://www.npmjs.com/package/homebridge-config-ui-x) or [Hoobs](https://hoobs.org/).
 3. Update your Homebridge `config.json` accordingly.
 
-
 ## Configuration
 
 NOTE: Don't forget to update `shelly_ip` to the IP address of your Shelly relay.
@@ -44,63 +43,66 @@ NOTE: Don't forget to update `shelly_ip` to the IP address of your Shelly relay.
         "statusURL": "http://shelly_ip/status",
         "statusKey": "$.inputs[0].input",
         "statusValueOpen": "0",
-        "statusValueClosed": "1"
+        "statusValueClosed": "1",
+        "debug": "false"
     }
 ]
 ```
 
-
 ## Options
 
 ### Core
-| Key | Description | Default |
-| --- | --- | --- |
-| `accessory` | Must be `GarageDoorOpener` | N/A |
-| `name` | Name to appear in the Home app | N/A |
-| `openURL` | URL to trigger the opening of your garage | N/A |
-| `closeURL` | URL to trigger the closing of your garage | N/A |
+
+| Key         | Description                               | Default |
+| ----------- | ----------------------------------------- | ------- |
+| `accessory` | Must be `GarageDoorOpener`                | N/A     |
+| `name`      | Name to appear in the Home app            | N/A     |
+| `openURL`   | URL to trigger the opening of your garage | N/A     |
+| `closeURL`  | URL to trigger the closing of your garage | N/A     |
 
 ### Optional fields
-| Key | Description | Default |
-| --- | --- | --- |
-| `openTime` | Time (in seconds) to simulate your garage opening | `10` |
-| `closeTime` | Time (in seconds) to simulate your garage closing | `10` |
-| `autoLock` | Whether your garage should auto-close after being opened | `false` |
-| `autoLockDelay` | Time (in seconds) until your garage will automatically close (if enabled) | `20` |
-| `switchOff` | Closes the garage immediately without animation. For IR remote control use. | `false` |
-| `switchOffDelay` | Time (in seconds) until your garage will automatically close without animation (if enabled) | `2` |
-| `polling` | Whether the state should be polled at intervals | `false` |
-| `pollInterval` | Time (in seconds) between device polls (if `polling` is enabled) | `120` |
-| `statusURL` | URL to retrieve state on poll (if `statusField*` options are not set, expects HTTP response body to be `0` or `1`) | N/A |
-| `statusKey` | [JSONPath](https://www.npmjs.com/package/jsonpath) that identifies the property that contains the status of the door (e.g. `$.inputs[0].input` is the default for Shelly 1) | `$.inputs[0].input` |
-| `statusValueOpen` | Regex that will match the `open` state of the relay status (e.g. `open`) | `0`  |
-| `statusValueClosed` | Regex that will match the `closed` state of the relay status (e.g. `closed`) | `1` |
-| `statusValueOpening` | Regex that will match the `opening` state of the relay status (e.g. `opening`) |  `2` |
-| `statusValueClosing` | Regex that will match the `closing` state of the relay status (e.g. `closing`) | `3` |
+
+| Key                  | Description                                                                                                                                                                 | Default             |
+| -------------------- | --------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | ------------------- |
+| `openTime`           | Time (in seconds) to simulate your garage opening                                                                                                                           | `10`                |
+| `closeTime`          | Time (in seconds) to simulate your garage closing                                                                                                                           | `10`                |
+| `autoLock`           | Whether your garage should auto-close after being opened                                                                                                                    | `false`             |
+| `autoLockDelay`      | Time (in seconds) until your garage will automatically close (if enabled)                                                                                                   | `20`                |
+| `switchOff`          | Closes the garage immediately without animation. For IR remote control use.                                                                                                 | `false`             |
+| `switchOffDelay`     | Time (in seconds) until your garage will automatically close without animation (if enabled)                                                                                 | `2`                 |
+| `polling`            | Whether the state should be polled at intervals                                                                                                                             | `false`             |
+| `pollInterval`       | Time (in seconds) between device polls (if `polling` is enabled)                                                                                                            | `120`               |
+| `statusURL`          | URL to retrieve state on poll (if `statusField*` options are not set, expects HTTP response body to be `0` or `1`)                                                          | N/A                 |
+| `statusKey`          | [JSONPath](https://www.npmjs.com/package/jsonpath) that identifies the property that contains the status of the door (e.g. `$.inputs[0].input` is the default for Shelly 1) | `$.inputs[0].input` |
+| `statusValueOpen`    | Regex that will match the `open` state of the relay status (e.g. `open`)                                                                                                    | `0`                 |
+| `statusValueClosed`  | Regex that will match the `closed` state of the relay status (e.g. `closed`)                                                                                                | `1`                 |
+| `statusValueOpening` | Regex that will match the `opening` state of the relay status (e.g. `opening`)                                                                                              | `2`                 |
+| `statusValueClosing` | Regex that will match the `closing` state of the relay status (e.g. `closing`)                                                                                              | `3`                 |
 
 ### Additional options
-| Key | Description | Default |
-| --- | --- | --- |
-| `timeout` | Time (in milliseconds) until the accessory will be marked as _Not Responding_ if it is unreachable | `3000` |
-| `http_method` | HTTP method used to communicate with the device | `GET` |
-| `username` | Username if HTTP authentication is enabled | N/A |
-| `password` | Password if HTTP authentication is enabled | N/A |
-| `model` | Appears under the _Model_ field for the accessory | plugin |
-| `serial` | Appears under the _Serial_ field for the accessory | version |
-| `manufacturer` | Appears under the _Manufacturer_ field for the accessory | author |
-| `firmware` | Appears under the _Firmware_ field for the accessory | version |
+
+| Key            | Description                                                                                        | Default |
+| -------------- | -------------------------------------------------------------------------------------------------- | ------- |
+| `timeout`      | Time (in milliseconds) until the accessory will be marked as _Not Responding_ if it is unreachable | `3000`  |
+| `http_method`  | HTTP method used to communicate with the device                                                    | `GET`   |
+| `username`     | Username if HTTP authentication is enabled                                                         | N/A     |
+| `password`     | Password if HTTP authentication is enabled                                                         | N/A     |
+| `model`        | Appears under the _Model_ field for the accessory                                                  | plugin  |
+| `serial`       | Appears under the _Serial_ field for the accessory                                                 | version |
+| `manufacturer` | Appears under the _Manufacturer_ field for the accessory                                           | author  |
+| `firmware`     | Appears under the _Firmware_ field for the accessory                                               | version |
+| `debug`        | Display debug messages on Homebridge log                                                           | false   |
 
 ### State key
 
 | State | Description |
-| --- | --- |
-| `0` | Open |
-| `1` | Closed |
-| `2` | Opening |
-| `3` | Closing |
+| ----- | ----------- |
+| `0`   | Open        |
+| `1`   | Closed      |
+| `2`   | Opening     |
+| `3`   | Closing     |
 
-
-## Wiring 
+## Wiring
 
 ![Shelly 1 wiring](https://savjee.be/uploads/2020-06-smart-garage-door-shelly-home-assistant/shelly-schematic-dc.png)
 
@@ -111,14 +113,11 @@ More information at https://savjee.be/2020/06/make-garage-door-opener-smart-shel
 - [Shelly1 Garage Door Control](https://www.youtube.com/watch?v=aV7gOWjia5w)
 - [Automate your Garage Door! The PERFECT First DIY Smart Home Project](https://www.youtube.com/watch?v=WEZUxXNiERQ)
 
-## Door open/closed sensor 
+## Door open/closed sensor
 
 In order to know for sure if your gate is open or closed you need to install a Reed Switch sensor connected between `L` and `SW` (order is irrelevant). These cost between €2 and €5.
 
 ![Reed Switch](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlGm8m0RQnE2NE15JjLc4KEOUdR0QghniwDQkSQjto3mPq9qPUVGmlrB5vBVWsL1sJlLU9sWAOs4Y&usqp=CAc)
-
-
-
 
 For Shelly 1 and a normally open reed switch (NO) the following options need to be set:
 
@@ -147,6 +146,3 @@ For a normally closed switch (NC), use:
 	  }
 	]
 ```
-
-
-
--- index.js
+++ index.js
@@ -3,263 +3,278 @@ const packageJson = require("./package.json");
 const request = require("request");
 const jp = require("jsonpath");
 
-module.exports = function (homebridge) {
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory(
-    "homebridge-garage-door-shelly1",
-    "GarageDoorOpener",
-    GarageDoorOpener
-  );
+module.exports = function(homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory(
+        "homebridge-garage-door-shelly1",
+        "GarageDoorOpener",
+        GarageDoorOpener
+    );
 };
 
 function GarageDoorOpener(log, config) {
-  this.log = log;
+    this.log = log;
 
-  this.name = config.name;
+    this.name = config.name;
 
-  this.openURL = config.openURL;
-  this.closeURL = config.closeURL;
+    this.openURL = config.openURL;
+    this.closeURL = config.closeURL;
 
-  this.openTime = config.openTime || 10;
-  this.closeTime = config.closeTime || 10;
+    this.openTime = config.openTime || 10;
+    this.closeTime = config.closeTime || 10;
 
-  this.switchOff = config.switchOff || false;
-  this.switchOffDelay = config.switchOffDelay || 2;
+    this.switchOff = config.switchOff || false;
+    this.switchOffDelay = config.switchOffDelay || 2;
 
-  this.autoLock = config.autoLock || false;
-  this.autoLockDelay = config.autoLockDelay || 20;
+    this.autoLock = config.autoLock || false;
+    this.autoLockDelay = config.autoLockDelay || 20;
 
-  this.manufacturer = config.manufacturer || packageJson.author.name;
-  this.serial = config.serial || packageJson.version;
-  this.model = config.model || packageJson.name;
-  this.firmware = config.firmware || packageJson.version;
+    this.manufacturer = config.manufacturer || packageJson.author.name;
+    this.serial = config.serial || packageJson.version;
+    this.model = config.model || packageJson.name;
+    this.firmware = config.firmware || packageJson.version;
 
-  this.username = config.username || null;
-  this.password = config.password || null;
-  this.timeout = config.timeout || 3000;
-  this.http_method = config.http_method || "GET";
+    this.username = config.username || null;
+    this.password = config.password || null;
+    this.timeout = config.timeout || 3000;
 
-  this.polling = config.polling || false;
-  this.pollInterval = config.pollInterval || 120;
-  this.statusURL = config.statusURL;
+    this.http_method = config.http_method || "GET";
 
-  this.statusKey = config.statusKey || "$.inputs[0].input";
-  this.statusValueOpen = config.statusValueOpen || "0";
-  this.statusValueClosed = config.statusValueClosed || "1";
-  this.statusValueOpening = config.statusValueOpening || "2";
-  this.statusValueClosing = config.statusValueClosing || "3";
+    this.polling = config.polling || false;
+    this.pollInterval = config.pollInterval || 120;
 
-  if (this.username != null && this.password != null) {
-    this.auth = {
-      user: this.username,
-      pass: this.password,
-    };
-  }
+    this.statusURL = config.statusURL;
+    this.statusKey = config.statusKey || "$.inputs[0].input";
 
-  this.service = new Service.GarageDoorOpener(this.name);
+    this.statusValueOpen = config.statusValueOpen || "0";
+    this.statusValueClosed = config.statusValueClosed || "1";
+    this.statusValueOpening = config.statusValueOpening || "2";
+    this.statusValueClosing = config.statusValueClosing || "3";
+
+    if (this.username != null && this.password != null) {
+        this.auth = {
+            user: this.username,
+            pass: this.password,
+        };
+    }
+
+    this.service = new Service.GarageDoorOpener(this.name);
 }
 
 GarageDoorOpener.prototype = {
-  identify: function (callback) {
-    this.log("Identify requested!");
-    callback();
-  },
+    identify: function(callback) {
+        this.log("Identify requested!");
+        callback();
+    },
 
-  _httpRequest: function (url, body, method, callback) {
-    request(
-      {
-        url: url,
-        body: body,
-        method: this.http_method,
-        timeout: this.timeout,
-        rejectUnauthorized: false,
-        auth: this.auth,
-      },
-      function (error, response, body) {
-        callback(error, response, body);
-      }
-    );
-  },
-
-  _getStatus: function (callback) {
-    var url = this.statusURL;
-    this.log.debug("Getting status: %s", url);
-
-    this._httpRequest(
-      url,
-      "",
-      "GET",
-      function (error, response, responseBody) {
-        if (error) {
-          this.log.warn("Error getting status: %s", error.message);
-          this.service
-            .getCharacteristic(Characteristic.CurrentDoorState)
-            .updateValue(new Error("Polling failed"));
-          callback(error);
-        } else {
-          let statusValue = 0;
-
-          if (this.statusKey) {
-            var originalStatusValue = jp
-              .query(
-                typeof responseBody === "string"
-                  ? JSON.parse(responseBody)
-                  : responseBody,
-                this.statusKey,
-                1
-              )
-              .pop();
-
-            if (new RegExp(this.statusValueOpen).test(originalStatusValue)) {
-              statusValue = 0;
-            } else if (
-              new RegExp(this.statusValueClosed).test(originalStatusValue)
-            ) {
-              statusValue = 1;
-            } else if (
-              new RegExp(this.statusValueOpening).test(originalStatusValue)
-            ) {
-              statusValue = 2;
-            } else if (
-              new RegExp(this.statusValueClosing).test(originalStatusValue)
-            ) {
-              statusValue = 3;
+    _httpRequest: function(url, body, method, callback) {
+        request({
+                url: url,
+                body: body,
+                method: this.http_method,
+                timeout: this.timeout,
+                rejectUnauthorized: false,
+                auth: this.auth,
+            },
+            function(error, response, body) {
+                callback(error, response, body);
             }
+        );
+    },
 
-            this.log.warn(
-              "Transformed status value from %s to %s (%s)",
-              originalStatusValue,
-              statusValue,
-              this.statusKey
+    _getStatus: function(callback) {
+        var url = this.statusURL;
+
+        if (this.config.debug) {
+            this.log.debug("Getting status: %s", url);
+        }
+
+        this._httpRequest(
+            url,
+            "",
+            "GET",
+            function(error, response, responseBody) {
+                if (error) {
+                    this.log.warn("Error getting status: %s", error.message);
+                    this.service
+                        .getCharacteristic(Characteristic.CurrentDoorState)
+                        .updateValue(new Error("Polling failed"));
+                    callback(error);
+                } else {
+                    let statusValue = 0;
+
+                    if (this.statusKey) {
+                        var originalStatusValue = jp
+                            .query(
+                                typeof responseBody === "string" ?
+                                JSON.parse(responseBody) :
+                                responseBody,
+                                this.statusKey,
+                                1
+                            )
+                            .pop();
+
+                        if (new RegExp(this.statusValueOpen).test(originalStatusValue)) {
+                            statusValue = 0;
+                        } else if (
+                            new RegExp(this.statusValueClosed).test(originalStatusValue)
+                        ) {
+                            statusValue = 1;
+                        } else if (
+                            new RegExp(this.statusValueOpening).test(originalStatusValue)
+                        ) {
+                            statusValue = 2;
+                        } else if (
+                            new RegExp(this.statusValueClosing).test(originalStatusValue)
+                        ) {
+                            statusValue = 3;
+                        }
+
+                        if (this.config.debug) {
+                            this.log.warn(
+                                "Transformed status value from %s to %s (%s)",
+                                originalStatusValue,
+                                statusValue,
+                                this.statusKey
+                            );
+                        }
+                    } else {
+                        statusValue = responseBody;
+                    }
+                    this.service
+                        .getCharacteristic(Characteristic.CurrentDoorState)
+                        .updateValue(statusValue);
+                    this.service
+                        .getCharacteristic(Characteristic.TargetDoorState)
+                        .updateValue(statusValue);
+
+                    if (this.config.debug) {
+                        this.warn("Updated door state to: %s", statusValue);
+                    }
+
+                    callback();
+                }
+            }.bind(this)
+        );
+    },
+
+    setTargetDoorState: function(value, callback) {
+        var url;
+
+        this.log.debug("Setting targetDoorState to %s", value);
+
+        if (value === 1) {
+            url = this.closeURL;
+        } else {
+            url = this.openURL;
+        }
+
+        this._httpRequest(
+            url,
+            "",
+            this.http_method,
+            function(error, response, responseBody) {
+                if (error) {
+                    this.log.warn("Error setting targetDoorState: %s", error.message);
+                    callback(error);
+                } else {
+                    if (value === 1) {
+                        this.log("Started closing");
+                        this.simulateClose();
+                    } else {
+                        this.log("Started opening");
+                        if (this.switchOff) {
+                            this.switchOffFunction();
+                        }
+                        if (this.autoLock) {
+                            this.autoLockFunction();
+                        }
+                        this.simulateOpen();
+                    }
+                    callback();
+                }
+            }.bind(this)
+        );
+    },
+
+    simulateOpen: function() {
+        this.service
+            .getCharacteristic(Characteristic.CurrentDoorState)
+            .updateValue(2);
+        setTimeout(() => {
+            this.service
+                .getCharacteristic(Characteristic.CurrentDoorState)
+                .updateValue(0);
+            this.log("Finished opening");
+        }, this.openTime * 1000);
+    },
+
+    simulateClose: function() {
+        this.service
+            .getCharacteristic(Characteristic.CurrentDoorState)
+            .updateValue(3);
+        setTimeout(() => {
+            this.service
+                .getCharacteristic(Characteristic.CurrentDoorState)
+                .updateValue(1);
+            this.log("Finished closing");
+        }, this.closeTime * 1000);
+    },
+
+    autoLockFunction: function() {
+        this.log("Waiting %s seconds for autolock", this.autoLockDelay);
+        setTimeout(() => {
+            this.service.setCharacteristic(Characteristic.TargetDoorState, 1);
+            this.log("Autolocking...");
+        }, this.autoLockDelay * 1000);
+    },
+
+    switchOffFunction: function() {
+        this.log("Waiting %s seconds for switch off", this.switchOffDelay);
+        setTimeout(() => {
+            this.log("SwitchOff...");
+            this._httpRequest(
+                this.closeURL,
+                "",
+                this.http_method,
+                function(error, response, responseBody) {}.bind(this)
             );
-          } else {
-            statusValue = responseBody;
-          }
-          this.service
-            .getCharacteristic(Characteristic.CurrentDoorState)
-            .updateValue(statusValue);
-          this.service
+        }, this.switchOffDelay * 1000);
+    },
+
+    getServices: function() {
+        this.informationService = new Service.AccessoryInformation();
+
+        this.informationService
+            .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+            .setCharacteristic(Characteristic.Model, this.model)
+            .setCharacteristic(Characteristic.SerialNumber, this.serial)
+            .setCharacteristic(Characteristic.FirmwareRevision, this.firmware);
+
+        this.service
             .getCharacteristic(Characteristic.TargetDoorState)
-            .updateValue(statusValue);
-          this.log("Updated state to: %s", statusValue);
-          callback();
-        }
-      }.bind(this)
-    );
-  },
+            .on("set", this.setTargetDoorState.bind(this));
 
-  setTargetDoorState: function (value, callback) {
-    var url;
-    this.log.debug("Setting targetDoorState to %s", value);
-    if (value === 1) {
-      url = this.closeURL;
-    } else {
-      url = this.openURL;
-    }
-    this._httpRequest(
-      url,
-      "",
-      this.http_method,
-      function (error, response, responseBody) {
-        if (error) {
-          this.log.warn("Error setting targetDoorState: %s", error.message);
-          callback(error);
+        if (this.polling) {
+            this._getStatus(function() {});
+
+            setInterval(
+                function() {
+                    this._getStatus(function() {});
+                }.bind(this),
+                this.pollInterval * 1000
+            );
         } else {
-          if (value === 1) {
-            this.log("Started closing");
-            this.simulateClose();
-          } else {
-            this.log("Started opening");
-            if (this.switchOff) {
-              this.switchOffFunction();
-            }
-            if (this.autoLock) {
-              this.autoLockFunction();
-            }
-            this.simulateOpen();
-          }
-          callback();
+            this.service
+                .getCharacteristic(Characteristic.CurrentDoorState)
+                .updateValue(1);
+
+            this.service
+                .getCharacteristic(Characteristic.TargetDoorState)
+                .updateValue(1);
         }
-      }.bind(this)
-    );
-  },
 
-  simulateOpen: function () {
-    this.service
-      .getCharacteristic(Characteristic.CurrentDoorState)
-      .updateValue(2);
-    setTimeout(() => {
-      this.service
-        .getCharacteristic(Characteristic.CurrentDoorState)
-        .updateValue(0);
-      this.log("Finished opening");
-    }, this.openTime * 1000);
-  },
-
-  simulateClose: function () {
-    this.service
-      .getCharacteristic(Characteristic.CurrentDoorState)
-      .updateValue(3);
-    setTimeout(() => {
-      this.service
-        .getCharacteristic(Characteristic.CurrentDoorState)
-        .updateValue(1);
-      this.log("Finished closing");
-    }, this.closeTime * 1000);
-  },
-
-  autoLockFunction: function () {
-    this.log("Waiting %s seconds for autolock", this.autoLockDelay);
-    setTimeout(() => {
-      this.service.setCharacteristic(Characteristic.TargetDoorState, 1);
-      this.log("Autolocking...");
-    }, this.autoLockDelay * 1000);
-  },
-
-  switchOffFunction: function () {
-    this.log("Waiting %s seconds for switch off", this.switchOffDelay);
-    setTimeout(() => {
-      this.log("SwitchOff...");
-      this._httpRequest(
-        this.closeURL,
-        "",
-        this.http_method,
-        function (error, response, responseBody) {}.bind(this)
-      );
-    }, this.switchOffDelay * 1000);
-  },
-
-  getServices: function () {
-    this.informationService = new Service.AccessoryInformation();
-    this.informationService
-      .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
-      .setCharacteristic(Characteristic.Model, this.model)
-      .setCharacteristic(Characteristic.SerialNumber, this.serial)
-      .setCharacteristic(Characteristic.FirmwareRevision, this.firmware);
-
-    this.service
-      .getCharacteristic(Characteristic.TargetDoorState)
-      .on("set", this.setTargetDoorState.bind(this));
-
-    if (this.polling) {
-      this._getStatus(function () {});
-
-      setInterval(
-        function () {
-          this._getStatus(function () {});
-        }.bind(this),
-        this.pollInterval * 1000
-      );
-    } else {
-      this.service
-        .getCharacteristic(Characteristic.CurrentDoorState)
-        .updateValue(1);
-      this.service
-        .getCharacteristic(Characteristic.TargetDoorState)
-        .updateValue(1);
-    }
-
-    return [this.informationService, this.service];
-  },
+        return [this.informationService, this.service];
+    },
 };
